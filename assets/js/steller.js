e0494236618aea46234b3bdea379c2f7c58b1872
Khairil Yusuf <khairilyusuf404@gmail.com>
2024-10-20 16:46:07 +0700

    MENAMBAHKAN ANIMASI SCROLL DAN MENGGANTI LOGO

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -1,35 +1,27 @@
-/*!
-=========================================================
-* Steller Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
 $(document).ready(function(){
-	$("#hire-me").on('click', function(event) {
-		$("#learn-more").on('click', function(event) {
+    $("#hire-me").on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
 
-    		if (this.hash !== "") {
+    $("#learn-more").on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#portfolio").offset().top
+        }, 1000);
+    });
 
-				event.preventDefault();
-
-				var hash = this.hash;
-
-				$('html, body').animate({
-				scrollTop: $(#"contact").offset().top
-				scrollTop: $(#"portfolio").offset().top
-				}, 700, function(){
-				window.location.hash = hash;
-				});
-			}	
-		});
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        }
     });
 });
